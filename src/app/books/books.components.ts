1d57aa4c7dc06f042f0024f6f0d8f81786431f76
A099653 <aron_chandrababu_2@progressive.com>
2024-11-07 20:05:11 -0500

    Final

--- src/app/books/books.components.ts
+++ src/app/books/books.components.ts
@@ -5,7 +5,14 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ``,
+  template: ` <div class="flex gap-8">
+    <div class="col-span-1">
+      <app-books-table [books]="store.currentPage()" />
+    </div>
+    <div class="col-span-1">
+      <app-books-by-century [books]="store.entities()" />
+    </div>
+  </div>`,
   styles: ``,
 })
 export class BooksComponents {}
--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { BooksComponents } from './books.components';
+import { BooksService } from './service/books.service';
+import { BooksStore } from './service/books.store';
 
 export const BOOKS_ROUTES: Routes = [
   {
     path: '',
     component: BooksComponents,
-    providers: [],
+    providers: [BooksService, BooksStore],
   },
 ];
